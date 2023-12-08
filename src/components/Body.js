53c7c666c523496f2edaa28a576a460441969727
imdeepakvermaa <dv24012002@gmail.com>
2023-12-08 16:58:48 +0530

    header part UI completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>
-      
+    <div className='bg-black'>
     </div>
   )
 }
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,11 +1,56 @@
-import React from 'react'
+// Head.js
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faMagnifyingGlass,
+  faMicrophone,
+  faVideo,
+  faBell,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Head = () => {
   return (
-    <div>
-      
+    <div className="m-2 p-2 text-white">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-4 md:gap-16 ml-8 mr-8 ">
+        <div className="flex gap-6">
+          <div>
+            <FontAwesomeIcon icon={faBars} className="h-5" />
+          </div>
+          <div>
+            <img
+              src="https://imgs.search.brave.com/gGkRJVq1V6RZri5qx1sKpiXZhyahy5o8pj6T8Mw2v_4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9i/L2I4L1lvdVR1YmVf/TG9nb18yMDE3LnN2/Zw.svg"
+              alt="Website-logo"
+              className="w-24 text-white"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-4 md:gap-8 items-center">
+          <div className="flex justify-center items-center">
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-full md:w-[500px] h-9 bg-gray-600 text-black rounded-l-2xl ring-0 focus:ring-0 shadow-none focus:shadow-none focus:outline-none border-none focus:border-none"
+            />
+            <div className="flex justify-center items-center w-10 h-9 border-gray-400 bg-gray-600 text-white rounded-r-2xl">
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="h-5" />
+            </div>
+          </div>
+          <div className="flex justify-center items-center">
+            <FontAwesomeIcon icon={faMicrophone} className="h-6" />
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center gap-4 md:gap-8">
+          <FontAwesomeIcon icon={faVideo} className="h-5" />
+          <FontAwesomeIcon icon={faBell} className="h-5" />
+          <FontAwesomeIcon icon={faUser} className="h-5" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Head;
