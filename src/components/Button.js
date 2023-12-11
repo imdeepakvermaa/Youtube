1e9a9bfce64cf2b6e2c7ad7c72243ed2bc5ccf1d
imdeepakvermaa <dv24012002@gmail.com>
2023-12-11 21:10:09 +0530

    button done

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button className="px-5 py-2 m-5 bg-gray-300 rounded-lg"></button>
+    </div>
+  );
+};
+
+export default Button;
