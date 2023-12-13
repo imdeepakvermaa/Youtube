2e42124c84ae2791c502c4cc7c84d731b10047c4
imdeepakvermaa <dv24012002@gmail.com>
2023-12-13 15:37:54 +0530

    almost done the watch page

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,25 +1,94 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import { Google_API_Key } from "../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
+  const [videoDetails, setVideoDetails] = useState({
+    title: "",
+    description: "",
+  },[]);
   const dispatch = useDispatch();
+
   useEffect(() => {
+    const fetchVideoDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${searchParams.get(
+            "v"
+          )}&key=${Google_API_Key}`
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch video details");
+        }
+
+        const data = await response.json();
+        const snippet = data.items[0].snippet;
+        const statistics = data.items[2].statistics;
+        setVideoDetails({
+          title: snippet.title,
+          description: snippet.description,
+          channelTitle: snippet.channelTitle,
+          ChannelDp: snippet.thumbnails.default.url,
+          likeCount: statistics.likeCount,
+        });
+      } catch (error) {
+        console.error("Error fetching video details:", error);
+      }
+    };
+
     dispatch(closeMenu());
-  }, []);
+    fetchVideoDetails();
+  }, [dispatch, searchParams]);
+
   return (
     <div className="text-white px-24 py-4">
-      <iframe className="rounded-2xl"
+      <iframe
+        className="rounded-2xl"
         width="900"
         height="500"
-        src={"https://www.youtube.com/embed/"+searchParams.get("v")}
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
+
+      <div>
+        <div>
+          <h2 className="text-xl font-bold ">{videoDetails.title}</h2>
+          {/* <p>{videoDetails.description}</p> */}
+        </div>
+
+        <div>
+          <div>
+            <div>
+              <img src={videoDetails.ChannelDp} alt="" />
+            </div>
+            <div>
+              <p className="text-white">{videoDetails.channelTitle}</p>
+              <p>9.89M subscriber</p>
+            </div>
+            <div>
+              <button className="text-black text-lg bg-white w-32 h-10 rounded-2xl font-semibold">
+                <FontAwesomeIcon icon={faBell} /> Subscribe
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <button>
+            <FontAwesomeIcon icon={faThumbsUp} />
+            </button> 
+            {videoDetails.likeCount}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const Google_API_Key = "AIzaSyAzrchD_u_7msClUiTsw5VmXYSzOohZ9NU";
+export const Google_API_Key = "AIzaSyAzrchD_u_7msClUiTsw5VmXYSzOohZ9NU";
 
 
 export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+Google_API_Key;
