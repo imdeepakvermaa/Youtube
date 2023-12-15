68ff6fa8dea1c4ccd60cf3076b31fce154e47d06
imdeepakvermaa <dv24012002@gmail.com>
2023-12-15 22:46:53 +0530

    made some changes

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,28 +1,35 @@
 import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import store from "../utils/store";
 
 const LiveChat = () => {
-    useEffect(() => {
-        const  i = setInterval(() => {
-            console.log("API polling");
+  const dispatch = useDispatch();
+  const ChatMessages = useSelector((store) => store.chat.messages);
+  useEffect(() => {
+    const i = setInterval(() => {
+      console.log("API polling");
+      dispatch(
+        addMessage({
+          name: "Deepak Verma",
+          message: "Making something cool 🚀",
+        })
+      );
+    }, 2000);
 
-        },2000);
-
-        return () => clearInterval(i);
-
-    },[]);      
+    return () => clearInterval(i);
+  }, []);
   return (
     <div className="text-white mt-3 ">
-      <div className="h-[500px] w-[400px] border border-gray-200 rounded-lg">
+      <div className="h-[500px] w-[400px] border border-gray-200 rounded-lg overflow-y-scroll">
         <div>
           <div>
             <h2 className="font-semibold text-base px-3 py-2 ">Live Chat</h2>
           </div>
-          <ChatMessage
-            name="Deepak Verma"
-            message="Your videos are very helpful thank you🙏"
-          />
-          
+            {ChatMessages.map((c, index) => (
+              <ChatMessage key={index} name={c.name} message={c.message} />
+            ))}
         </div>
       </div>
     </div>
