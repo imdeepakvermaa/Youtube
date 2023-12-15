44c44f4be46c8b0c270391f630960dd338150540
imdeepakvermaa <dv24012002@gmail.com>
2023-12-15 20:13:44 +0530

    name and message applied to the function

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const ChatMessage = ({name,message}) => {
+const ChatMessage = ({ name, message }) => {
   return (
-    <div>
-        <img src="https://imgs.search.brave.com/9IEl1PF7dviJo9V_Hf6HnBZmDE6Dx7SnKUsz62u5ddM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8x/LzEyL1VzZXJfaWNv/bl8yLnN2Zw.svg" alt="" />
-
-
-        <span>{name}</span>
-        <span>{message}</span>
-      
+    <div className="">
+      <div className="flex item-center gap-1 font-bold">
+        <img
+          className="w-7 h-7"
+          src="https://imgs.search.brave.com/9IEl1PF7dviJo9V_Hf6HnBZmDE6Dx7SnKUsz62u5ddM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8x/LzEyL1VzZXJfaWNv/bl8yLnN2Zw.svg"
+          alt="profile"
+        />
+        <div>
+          <span className="text-sm">{name}</span>
+        </div>
+      </div>
+      <span className="text-sm px-8">{message}</span>
     </div>
-  )
-}
+  );
+};
 
 export default ChatMessage;
--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from "react";
+import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
   return (
-    <div className='text-white'>
-      <div className='h-[500px] w-[400px] border border-gray-200 '>
+    <div className="text-white mt-3 ">
+      <div className="h-[500px] w-[400px] border border-gray-200 rounded-lg">
         <div>
-            <h2 className='font-semibold text-base px-3 py-2 '>Live Chat</h2>
+          <div>
+            <h2 className="font-semibold text-base px-3 py-2 ">Live Chat</h2>
+          </div>
+          <ChatMessage
+            name="Deepak Verma"
+            message="Your videos are very helpful thank you🙏"
+          />
+          <ChatMessage
+            name="Deepak Verma"
+            message="Your videos are very helpful thank you🙏"
+          />
+          <ChatMessage
+            name="Deepak Verma"
+            message="Your videos are very helpful thank you🙏"
+          />
+          <ChatMessage
+            name="Deepak Verma"
+            message="Your videos are very helpful thank you🙏"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LiveChat;
