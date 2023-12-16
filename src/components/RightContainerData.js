79bf73d212febc81b445f2b18689f7d6845a09b1
imdeepakvermaa <dv24012002@gmail.com>
2023-12-16 18:58:00 +0530

    scrollbar scenes left

--- src/components/RightContainerData.js
+++ src/components/RightContainerData.js
@@ -1,3 +1,16 @@
+import React from 'react';
+
+// Function to format view count
+const formatViewCount = (count) => {
+  if (count >= 1e6) {
+    return `${(count / 1e6).toFixed(1)}M`;
+  } else if (count >= 1e3) {
+    return `${(count / 1e3).toFixed(1)}K`;
+  } else {
+    return count.toString();
+  }
+};
+
 const RightContainerData = ({ info }) => {
   if (!info) {
     return <div className="text-white">Loading...</div>;
@@ -7,14 +20,14 @@ const RightContainerData = ({ info }) => {
   const { channelTitle, title, thumbnails } = snippet;
 
   return (
-    <div className=" w-[400px] ">
-      <div className="flex flex-col ">
+    <div className="w-400px">
+      <div className="flex flex-col">
         <img
-          className="h-[290px] w-[400px]rounded-xl"
+          className="h-[290px] w-[400px] rounded-xl"
           src={thumbnails?.high?.url}
           alt="thumbnail"
         />
-        <div className="flex justify-center items-center gap-3 -mt-9">
+        <div className="flex justify-center items-center gap-3 -mt-7">
           <img
             className="h-[40px] w-10 rounded-[600px]  "
             src={thumbnails?.high?.url}
@@ -31,7 +44,7 @@ const RightContainerData = ({ info }) => {
       <div className="flex items-center ml-12">
         <div className="text-gray-300">
           <p>{channelTitle}</p>
-          <p>{statistics.viewCount} Views</p>
+          <p>{formatViewCount(statistics.viewCount)} Views</p>
         </div>
       </div>
     </div>
